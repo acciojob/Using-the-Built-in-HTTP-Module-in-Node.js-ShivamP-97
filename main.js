8b13831b224fb60f2732e55e7ce5c56ea03012e8
= <shivampathak19972@gmail.com>
2026-01-28 22:35:28 +0530

    Test

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// TODO: Create an HTTP server
+const filePath = process.argv[2];
 
-// TODO: Listen on port 3000
+const server = http.createServer((req, res) => {
+  if (!filePath) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    return res.end('Error: No file path provided.');
+  }
 
-// DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
+  fs.readFile(path.resolve(filePath), 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      return res.end('Error reading file.');
+    }
 
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(data);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
