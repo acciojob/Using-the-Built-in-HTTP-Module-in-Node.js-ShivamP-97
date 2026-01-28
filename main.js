ee6774ab9057b3cc1f2a1f42b077153933ae1c13
= <shivampathak19972@gmail.com>
2026-01-28 22:37:25 +0530

    Test

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+  });
+}
+
+module.exports = server;
